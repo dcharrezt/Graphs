154e44a9a3792cde72def423e032d81f5aca5a90
faoladhaku <angelsoulsword@gmail.com>
2016-12-03 00:43:33 -0500

    ayudaaagit add *git add *

--- build/CMakeFiles/Graphs.dir/DependInfo.cmake
+++ build/CMakeFiles/Graphs.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/eduardob/Desktop/AED_Project/Graphs/main.cpp" "/home/eduardob/Desktop/AED_Project/Graphs/build/CMakeFiles/Graphs.dir/main.cpp.o"
+  "/home/eduardob/Desktop/AED_Project/main.cpp" "/home/eduardob/Desktop/AED_Project/build/CMakeFiles/Graphs.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/eduardob/Desktop/AED_Project/Graphs
+# Install script for directory: /home/eduardob/Desktop/AED_Project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/eduardob/Desktop/AED_Project/Graphs/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/eduardob/Desktop/AED_Project/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
